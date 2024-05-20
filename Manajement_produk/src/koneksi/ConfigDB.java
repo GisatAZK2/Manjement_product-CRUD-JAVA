109001289fc6accb4dc305b20ebd3ab5c859d0bd
GisatAZK2 <147264875+GisatAZK2@users.noreply.github.com>
2024-05-21 06:45:01 +0700

    Add files via upload

---
+++ Manajement_produk/src/koneksi/ConfigDB.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package koneksi;
+
+/**
+ *
+ * @author Administrator
+ */
+import javax.swing.JOptionPane;
+import java.sql.Connection;
+import java.sql.DriverManager;
+public class ConfigDB {
+    private static Connection mysqlconnection;
+    public static Connection mydatabaseconnection(){
+    try{
+    String DBUrl ="jdbc:mysql://localhost:3306/db_manajemen";
+    String DBUser="root";
+    String DBpass="";
+    mysqlconnection=(Connection)DriverManager.getConnection(DBUrl,DBUser,DBpass);
+ 
+    }catch(Exception e){
+         JOptionPane.showMessageDialog(null,"Connection Error");
+    }
+    return mysqlconnection;
+    }
+}
